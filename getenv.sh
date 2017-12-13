e9e1a9c7b73717a08a87c61087c3bc81c737cd30
Johannes <johannes@jitesoft.com>
2017-12-13 19:26:02 +0100

    Well...

--- getenv.sh
+++ getenv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env ash
 VARIABLE=$1
 IMAGE=$2
-echo $(docker inspect -f '{{range $index, $value := .Config.Env}}{{if eq (index (split $value "=") 0) "${VARIABLE}" }}{{range $i, $part := (split $value "=")}}{{if gt $i 1}}{{print "="}}{{end}}{{if gt $i 0}}{{print $part}}{{end}}{{end}}{{end}}{{end}}' $IMAGE)
+echo $(docker inspect -f '{{range $index, $value := .Config.Env}}{{if eq (index (split $value "=") 0) "${VARIABLE}" }}{{range $i, $part := (split $value "=")}}{{if gt $i 1}}{{print "="}}{{end}}{{if gt $i 0}}{{print $part}}{{end}}{{end}}{{end}}{{end}}' ${IMAGE})
