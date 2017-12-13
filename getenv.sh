3f7aaa98cece1a469763c3fc0f353c106d45e04d
Johannes <johannes@jitesoft.com>
2017-12-13 18:26:31 +0100

    Fix for script calling.

--- getenv.sh
+++ getenv.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 VARIABLE=$1
 CONTAINER=$2
 echo docker inspect -f '{{range $index, $value := .Config.Env}}{{if eq (index (split $value "=") 0) "${VARIABLE}" }}{{range $i, $part := (split $value "=")}}{{if gt $i 1}}{{print "="}}{{end}}{{if gt $i 0}}{{print $part}}{{end}}{{end}}{{end}}{{end}}' $CONTAINER
